0f93e6101409a78c5609ed5346e205a99e9a6c21
gasperpb <achillespb@hotmail.com>
2022-07-08 17:30:02 -0300

    cabeçario

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
-
-import Image from 'next/image'
-import { Spinner, Stack } from '@chakra-ui/react'
-import {
-  Stat,
-  StatLabel,
-  StatNumber,
-  StatHelpText,
-  StatArrow,
-  StatGroup,
-} from '@chakra-ui/react'
-import Header from '../components/Header'
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-<Header/>
-  )
+    <>
+      <Box fontFamily="Montserrat" color="black">
+        <Flex height="133px" background="#8F2C48">
+          <Flex align="center" justify="center" width="100%">
+            <Text
+              fontSize="27px"
+              color="#fff000"
+              fontWeight="700"
+              textAlign="center"
+            >
+              ADQUIRA A <br />
+              GRAVAÇÃO NA
+              <br />
+              INTEGRA
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
 }
