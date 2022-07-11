fd546e7434b4e81ac5a8893aab434387a5250f77
gasperpb <achillespb@hotmail.com>
2022-07-11 19:09:46 -0300

    Projeto pronto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,1 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
 
+function MyApp({ Component, pagePros }) {
+  return (
+    <ChakraProvider>
+      <Component {...pagePros} />
+    </ChakraProvider>
+  );
+}
+export default MyApp;
