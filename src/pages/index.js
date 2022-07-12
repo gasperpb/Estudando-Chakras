9e886d3f5cf85d70cbec318e3cc73e9bfaa888ee
gasperpb <achillespb@hotmail.com>
2022-07-12 12:20:10 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,11 @@
-import Sidebar from "../components/Sidebar";
-import { Flex, Text, IconButton } from "@chakra-ui/react";
-import { FiMenu } from "react-icons/fi";
+import React from "react";
+import App from "./_app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex w="100%">
-      <Sidebar />
-      <Flex
-        pos="absolute"
-        top="50%"
-        left="50%"
-        transform="translate(-50%, -50%)"
-      >
-        <Text>
-          Click the
-          <IconButton
-            background="none"
-            _hover={{ background: "none" }}
-            icon={<FiMenu />}
-          />
-          to resize the vertical navigation bar.
-        </Text>
-      </Flex>
-    </Flex>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   );
 }
